8b2ae5a2663eded65fb23ad5bade1e2d21c81662
Jordy Aaldering <aalderingjordy@gmail.com>
2019-10-09 16:16:45 +0200

    Input Reading

--- Practicum1_Problem1/main.cpp
+++ Practicum1_Problem1/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int num_pis, num_cons;
+    std::cin >> num_pis >> num_cons;
+
+    int cons[num_cons][2];
+    for (auto& con : cons) {
+        std::cin >> con[0] >> con[1];
+    }
+
     return 0;
 }
