a4bc9f717500adfa0d96120e5c442520aee7ef25
Jordy Aaldering <aalderingjordy@gmail.com>
2019-10-22 00:06:51 +0200

    Shit
    depthutil loops through G/CCS inside but the callee also loops through G/CCS

--- Practicum1_Problem1/Graph.cpp
+++ Practicum1_Problem1/Graph.cpp
@@ -5,19 +5,75 @@ class Graph
 {
 private:
     int V; // amount of vertices
+    int E; // amount of edges
     std::vector<int>* G; // the graph
+
+    bool *visited;
 
     int CCCount = 0; // amount of connected components
     std::vector<int>* CCS; // the connected components
-    int* CCRoots; // connected component vertices with most neighbors
+    int* CCRoots; // cc vertices with most neighbors
+    int* CCRootDepths; // maximum depth of each cc root
 
+    int longestPath = 0;
+
+    void ccsUtil(int i)
+    {
+        CCS[CCCount].push_back(i);
+
+        visited[i] = true;
+        std::vector<int>::iterator j;
+
+        for (j = G[i].begin(); j != G[i].end(); j++) {
+            if (!visited[*j]) {
+                ccsUtil(*j);
+            }
+        }
+    }
+
+    int rootsUtil(int i, int depth = 0)
+    {
+        int newBestDepth = -1;
+
+        visited[i] = true;
+        std::vector<int>::iterator j;
+
+        for (j = CCS[i].begin(); j != CCS[i].end(); j++) {
+            if (!visited[*j]) {
+                int nextDepth = depthUtil(*j, depth + 1);
+                newBestDepth = std::max(newBestDepth, nextDepth);
+            }
+        }
+
+        return std::max(depth, newBestDepth);
+    }
+
+    int depthUtil(int i, int depth = 0)
+    {
+        int newBestDepth = -1;
+
+        visited[i] = true;
+        std::vector<int>::iterator j;
+
+        for (j = G[i].begin(); j != G[i].end(); j++) {
+            if (!visited[*j]) {
+                int nextDepth = depthUtil(*j, depth + 1);
+                newBestDepth = std::max(newBestDepth, nextDepth);
+            }
+        }
+
+        return std::max(depth, newBestDepth);
+    }
 
 public:
-    explicit Graph(int v)
+    explicit Graph(int v, int e)
     {
         this->V = v;
+        this->E = e;
         this->G = new std::vector<int>[v];
         this->CCS = new std::vector<int>[v];
+
+        this->visited = new bool[V];
     }
 
     void addEdge(int a, int b)
@@ -26,29 +82,15 @@ public:
         G[b].push_back(a);
     }
 
-    void dfsUtil(int i, bool *visited)
-    {
-        CCS[CCCount].push_back(i);
-
-        visited[i] = true;
-        std::vector<int>::iterator j;
-        for (j = G[i].begin(); j != G[i].end(); ++j) {
-            if (!visited[*j]) {
-                dfsUtil(*j, visited);
-            }
-        }
-    }
-
     void calculateCCS()
     {
-        bool *visited = new bool[V];
         for (int i = 0; i < V; i++) {
             visited[i] = false;
         }
 
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
-                dfsUtil(i, visited);
+                ccsUtil(i);
                 CCCount++;
             }
         }
@@ -59,18 +101,70 @@ public:
         CCRoots = new int[CCCount];
         for (int i = 0; i < CCCount; i++) {
             int bestRootIndex = -1;
-            int bestRootSize = -1;
+            int bestRootDepth = 1000000;
 
             for (int j : CCS[i]) {
-                int size = G[j].size();
-                if (size > bestRootSize) {
+                for (int k = 0; k < V; k++) {
+                    visited[k] = false;
+                }
+
+                int depth = depthUtil(j);
+                if (depth < bestRootDepth) {
                     bestRootIndex = j;
-                    bestRootSize = size;
+                    bestRootDepth = depth;
                 }
             }
 
             CCRoots[i] = bestRootIndex;
         }
+    }
+
+    void calculateCCRootDepths()
+    {
+        CCRootDepths = new int[CCCount];
+
+        for (int i = 0; i < CCCount; i++) {
+            for (int j = 0; j < V; j++) {
+                visited[j] = false;
+            }
+
+            CCRootDepths[i] = depthUtil(CCRoots[i]);
+        }
+    }
+
+    void extendGraph()
+    {
+        int longestDepthIndex = -1;
+        int longestDepthSize = -1;
+        for (int i = 0; i < CCCount; i++) {
+            if (CCRootDepths[i] > longestDepthSize) {
+                longestDepthIndex = CCRoots[i];
+                longestDepthSize = CCRootDepths[i];
+            }
+        }
+
+        for (int i = 0; i < CCCount; i++) {
+            if (CCRoots[i] != longestDepthIndex) {
+                addEdge(CCRoots[i], longestDepthIndex);
+            }
+        }
+    }
+
+    int calculateLongestPath()
+    {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                visited[j] = false;
+            }
+
+            for (int j : G[i])
+            {
+                int depth = depthUtil(j) - 1;
+                longestPath = std::max(longestPath, depth);
+            }
+        }
+
+        return longestPath;
     }
 
     void print()
@@ -96,5 +190,11 @@ public:
         for (int i = 0; i < CCCount; i++) {
             std::cout << "CC root " << i << ": " << CCRoots[i] << std::endl;
         }
+
+        for (int i = 0; i < CCCount; i++) {
+            std::cout << "CC depth " << i << ": " << CCRootDepths[i] << std::endl;
+        }
+
+        std::cout << "Longest path: " << longestPath << std::endl;
     }
 };
--- Practicum1_Problem1/main.cpp
+++ Practicum1_Problem1/main.cpp
@@ -6,7 +6,7 @@ Graph readFromUser()
     int V, E;
     std::cin >> V >> E;
 
-    Graph G(V);
+    Graph G(V, E);
     for (int i = 0; i < E; i++) {
         int c1, c2;
         std::cin >> c1 >> c2;
@@ -23,23 +23,65 @@ Graph readFromFile(const std::string& filename)
     int V, E;
     fs >> V >> E;
 
-    Graph G(V);
+    Graph G(V, E);
     int c1, c2;
     while (fs >> c1 >> c2) {
         G.addEdge(c1, c2);
     }
 
+    fs.close();
+
     return G;
+}
+
+int readExpected(const std::string& filename)
+{
+    std::fstream fs("..\\samples\\" + filename + ".out");
+
+    int expected;
+    fs >> expected;
+    fs.close();
+
+    return expected;
+}
+
+void checkFile(const std::string& i)
+{
+    std::string filename = "big_" + i;
+
+    std::cout << "Creating Graph..." << std::endl;
+    Graph G = readFromFile(filename);
+
+    std::cout << "Calculating CCS..." << std::endl;
+    G.calculateCCS();
+
+    std::cout << "Calculating Roots..." << std::endl;
+    G.calculateCCRoots();
+
+    std::cout << "Calculating Depths..." << std::endl;
+    G.calculateCCRootDepths();
+
+    std::cout << "Extending Graph..." << std::endl;
+    G.extendGraph();
+
+    std::cout << "Calculating Path..." << std::endl;
+    int length = G.calculateLongestPath();
+
+    //G.print();
+
+    int expected = readExpected(filename);
+    std::cout << i << ") " << length << "\t" << expected << std::endl;
 }
 
 int main()
 {
-    Graph G = readFromFile("small_11");
+    //std::string is[10] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    std::string is[1] = { "4" };
 
-    G.calculateCCS();
-    G.calculateCCRoots();
-
-    G.print();
+    std::cout << "Clcltd\tXpctd" << std::endl;
+    for (const std::string& i : is) {
+        checkFile(i);
+    }
 
     return 0;
 }
