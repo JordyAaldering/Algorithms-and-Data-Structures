c2193671857027113907b5e2285f7517618ecb10
Jordy Aaldering <aalderingjordy@gmail.com>
2019-11-29 11:12:22 +0100

    Input Reading Prepare

--- Practicum2/main.cpp
+++ Practicum2/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+using namespace std;
+
+void readStdIn() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        string actress;
+        cin >> actress;
+    }
+
+    for (int i = 0; i < n; i++) {
+        string actor;
+        cin >> actor;
+    }
+
+    for (int i = 0; i < m; i++) {
+        string movie;
+        cin >> movie;
+
+        int s;
+        cin >> s;
+
+        for (int j = 0; j < s; j++) {
+            string name;
+            cin >> name;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    readStdIn();
     return 0;
 }
