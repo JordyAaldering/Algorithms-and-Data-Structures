946b842bf9c42e27bbc171beaef08fb5b0084c8c
Jordy Aaldering <aalderingjordy@gmail.com>
2020-01-07 15:01:17 +0100

    Moved code to main

--- Practicum2/main.cpp
+++ Practicum2/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <fstream>
 
 using String = std::string;
 using Vertex = std::vector<int>;
@@ -15,10 +14,8 @@ void addEdge(int a, int b) {
     graph[b].push_back(a);
 }
 
-String readStdIn(const String& filename) {
-    std::fstream fs("..\\samples\\" + filename + ".in");
-
-    fs >> n >> m;
+void readStdIn() {
+    std::cin >> n >> m;
     vertexCount = n + n;
 
     graph = new Vertex[vertexCount];
@@ -27,30 +24,30 @@ String readStdIn(const String& filename) {
     // Keep track of actresses.
     for (int i = 0; i < n; i++) {
         String actress;
-        fs >> actress;
+        std::cin >> actress;
         persons[actress] = std::make_pair(actress, std::make_pair(i, false));
     }
 
     // Keep track of actors.
     for (int i = 0; i < n; i++) {
         String actor;
-        fs >> actor;
+        std::cin >> actor;
         persons[actor] = std::make_pair(actor, std::make_pair(n + i, true));
     }
 
     // Add casts of all movies.
     for (int i = 0; i < m; i++) {
         String movie;
-        fs >> movie;
+        std::cin >> movie;
 
         int s;
-        fs >> s;
+        std::cin >> s;
         Person cast[s];
 
         // Add all stars of a movie.
         for (int j = 0; j < s; j++) {
             String name;
-            fs >> name;
+            std::cin >> name;
 
             Person star = persons[name];
             cast[j] = star;
@@ -64,11 +61,6 @@ String readStdIn(const String& filename) {
             }
         }
     }
-
-    fs = std::fstream("..\\samples\\" + filename + ".out");
-    String expected;
-    fs >> expected;
-    return expected;
 }
 
 bool bpm(int index, bool* visited, int* assigned) {
@@ -103,36 +95,12 @@ int bpmMax() {
     return result;
 }
 
-bool allBpmContain(int v) {
-    // something
-
-    return true;
-}
-
 int main() {
     std::iostream::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    String a[] = { "a1", "a2" };
-    String b[] = { "b1", "b2" };
-    String v[] = { "v1", "v2", "v3", "v4" };
-    String mm[] = { "m1", "m2", "m3", "m4" };
-    String r[] = { "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10" };
-    String all[] = { "a1", "a2", "b1", "b2", "v1", "v2", "v3", "v4", "m1", "m2", "m3", "m4", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10" };
-
-    for (const String& filename : all) {
-        std::cout << "Expecting " << readStdIn(filename) << " got ";
-
-        bool win = false;
-        for (int i = 0; i < n; i++) {
-            if (allBpmContain(i)) {
-                win = true;
-                break;
-            }
-        }
-
-        std::cout << (win ? "Veronique" : "Mark") << std::endl;
-    }
+    readStdIn();
+    std::cout << (bpmMax() != n ? "Veronique" : "Mark") << std::endl;
 
     return 0;
 }
