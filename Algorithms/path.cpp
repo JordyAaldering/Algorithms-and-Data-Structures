8b1241663bb4503f469a3e83b5c38f36d6dbe9c8
Jordy Aaldering <aalderingjordy@gmail.com>
2020-01-17 21:03:54 +0100

    bellmanFord

--- Algorithms/path.cpp
+++ Algorithms/path.cpp
@@ -25,12 +25,31 @@ void dijkstra(int** graph, int size, int src = 0)
     std::fill(isSet, isSet + size, false);
 
     for (int step = 0; step < size - 1; step++) {
-        int i = minDistance(distance, isSet, size);
-        isSet[i] = true;
+        int u = minDistance(distance, isSet, size);
+        isSet[u] = true;
 
         for (int v = 0; v < size; v++) {
-            if (!isSet[v] && graph[i][v] && distance[i] < INT_MAX) {
-                int currentDistance = distance[i] + graph[i][v];
+            if (!isSet[v] && graph[u][v] && distance[u] < INT_MAX) {
+                int currentDistance = distance[u] + graph[u][v];
+
+                if (currentDistance < distance[v]) {
+                    distance[v] = currentDistance;
+                }
+            }
+        }
+    }
+}
+
+void bellmanFord(int** graph, int size, int src = 0) {
+    int distance[size];
+    std::fill(distance, distance + size, INT_MAX);
+    distance[src] = 0;
+
+    for (int step = 0; step < size - 1; step++) {
+        for (int u = 0; u < size; u++)
+        for (int v = 0; v < size; v++) {
+            if (graph[u][v] && distance[u] < INT_MAX) {
+                int currentDistance = distance[u] + graph[u][v];
 
                 if (currentDistance < distance[v]) {
                     distance[v] = currentDistance;
